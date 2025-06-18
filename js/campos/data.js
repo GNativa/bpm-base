0ac05cd17a36354e04e24a78d5ced3ed561229de
Nativa <92101458+GNativa@users.noreply.github.com>
2025-06-18 11:37:00 -0400

    - Implementação de data inicial para campos do tipo data.

--- js/campos/data.js
+++ js/campos/data.js
@@ -1,6 +1,26 @@
 class CampoData extends CampoEntrada {
-    constructor(id, rotulo, largura, dica, fonte, campoFonte) {
+    dataInicial;
+
+    constructor(id, rotulo, largura, dica, fonte, campoFonte, usarDataInicial, dataInicial) {
         super(id, rotulo, largura, dica, "date", fonte, campoFonte);
         this.inicializar();
+
+        if (usarDataInicial) {
+            this.dataInicial = dataInicial;
+            this.definirDataInicial();
+        }
+    }
+
+    definirDataInicial() {
+        let data;
+
+        if (!this.dataInicial) {
+            data = new Date();
+        }
+        else {
+            data = this.dataInicial;
+        }
+
+        this.val(data.toISOString().slice(0, 10));
     }
 }
