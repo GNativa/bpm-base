33b6ff6fe2d1d4c16f5f444f0932b040bd35f2a8
Nativa <92101458+GNativa@users.noreply.github.com>
2025-07-02 11:27:51 -0400

    - Atualização do campo de tipo lista para permitir listas com seleção de múltiplas opções.

--- js/campos/lista.js
+++ js/campos/lista.js
@@ -1,12 +1,17 @@
 class CampoLista extends Campo {
-    constructor(id, rotulo, largura, dica, fonte, campoFonte) {
+    #opcoes = [];
+    #variasOpcoes = false;
+
+    constructor(id, rotulo, largura, dica, fonte, campoFonte, variasOpcoes) {
         super(id, rotulo, largura, dica, "select", null, fonte, campoFonte);
+        this.#variasOpcoes = variasOpcoes ?? this.#variasOpcoes;
         this.inicializar();
     }
 
     configurarCampo() {
         this.classes.push("form-select");
         this.campo.ariaLabel = this.rotulo;
+        this.campo.prop("multiple", this.#variasOpcoes);
 
         const opcao = $("<option></option>");
         opcao.val("");
@@ -16,15 +21,29 @@ class CampoLista extends Campo {
         this.coluna.append(this.campo);
     }
 
-    adicionarOpcoes(opcoes) {
+    get variasOpcoes() {
+        return this.#variasOpcoes;
+    }
+
+    get opcoes() {
+        return Object.freeze(this.#opcoes);
+    }
+
+    adicionarOpcoes(opcoes = [new OpcaoLista()]) {
         const campo = this.campo;
 
         for (const opcao of opcoes) {
-            const elementoOpcao = $("<option></option>");
-            elementoOpcao.val(opcao["valor"]);
-            elementoOpcao.text(opcao["conteudo"]);
-            campo.append(elementoOpcao);
+            this.#opcoes.push(opcao);
+            campo.append(opcao.elemento);
         }
+
+        /*
+        campo.on("change", () => {
+            for (const opcao of this.#opcoes) {
+                opcao.selecionada = opcao.elemento.is(":selected");
+            }
+        });
+         */
 
         return this;
     }
@@ -36,8 +55,28 @@ class CampoLista extends Campo {
  */
 
 class OpcaoLista {
+    #valor = "";
+    #conteudo = "";
+    #elemento = $(`<option></option>`);
+    selecionada = false;
+
     constructor(valor, conteudo) {
-        this.valor = valor;
-        this.conteudo = conteudo;
+        this.#valor = valor;
+        this.#conteudo = conteudo;
+
+        this.#elemento.val(valor);
+        this.#elemento.text(conteudo);
+    }
+
+    get elemento() {
+        return this.#elemento;
+    }
+
+    get valor() {
+        return Object.freeze(this.#valor);
+    }
+
+    get conteudo() {
+        return Object.freeze(this.#conteudo);
     }
 }
